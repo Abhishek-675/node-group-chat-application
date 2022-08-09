14997967b640dadbf7de3e0b3768cbd568b0c542
Abhishek <dekaa331@gmail.com>
2022-08-09 19:21:15 +0530

    signup page added

---
+++ frontend/sign-up/script.js
@@ -0,0 +1,11 @@
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const password = document.getElementById('password');
+const button = document.querySelector('button');
+const form = document.querySelector('form');
+
+
+function signup(e) {
+    e.preventDefault();
+}
